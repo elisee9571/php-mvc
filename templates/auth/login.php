7a2b5d54066e88ba06f4d2d3aa2d5c52d564f759
Elisee Desmarest <elisee.desmarest@gmail.com>
2026-01-27 13:26:34 +0100

    second commit

--- templates/auth/login.php
+++ templates/auth/login.php
@@ -1,15 +1,17 @@
-<h1><?= $title ?></h1>
+<div class="w-100 d-flex flex-column align-items-center justify-content-center">
+    <h1><?= $title ?></h1>
 
-<form action="" method="post">
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input id="email" name="email" type="email" class="form-control" placeholder="exemple@gmail.com">
-    </div>
+    <form action="" method="post" class="w-50">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input id="email" name="email" type="email" class="form-control" placeholder="exemple@gmail.com">
+        </div>
 
-    <div class="mb-3">
-        <label for="password" class="form-label">Mot de passe</label>
-        <input id="password" name="password" type="password" class="form-control" placeholder="************">
-    </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input id="password" name="password" type="password" class="form-control" placeholder="************">
+        </div>
 
-    <button type="submit" class="btn btn-primary">Se connecter</button>
-</form>
+        <button type="submit" class="btn btn-primary">Se connecter</button>
+    </form>
+</div>
--- templates/auth/register.php
+++ templates/auth/register.php
@@ -1,20 +1,22 @@
-<h1><?= $title ?></h1>
+<div class="w-100 d-flex flex-column align-items-center justify-content-center">
+    <h1><?= $title ?></h1>
 
-<form action="" method="post">
-    <div class="mb-3">
-        <label for="username" class="form-label">Username</label>
-        <input id="username" name="username" type="text" class="form-control" placeholder="Exemple78">
-    </div>
+    <form action="" method="post" class="w-50">
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input id="username" name="username" type="text" class="form-control" placeholder="Exemple78">
+        </div>
 
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input id="email" name="email" type="email" class="form-control" placeholder="exemple@gmail.com">
-    </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input id="email" name="email" type="email" class="form-control" placeholder="exemple@gmail.com">
+        </div>
 
-    <div class="mb-3">
-        <label for="password" class="form-label">Mot de passe</label>
-        <input id="password" name="password" type="password" class="form-control" placeholder="************">
-    </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input id="password" name="password" type="password" class="form-control" placeholder="************">
+        </div>
 
-    <button type="submit" class="btn btn-primary">S'inscrire</button>
-</form>
+        <button type="submit" class="btn btn-primary">S'inscrire</button>
+    </form>
+</div>
